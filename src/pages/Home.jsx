bd550c71ba742931f1909437656a318c61f8834f
ziiemli <ziiemli@outlook.fr>
2023-08-31 02:19:12 +0200

    home desktop responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
 
     return (
         <main className="home">
-            <Header />
             <section className="home__top">
+                <Header />
                 <picture>
                     <source media="(min-width: 996px)" srcSet={"/src/assets/Home/Home_D.jpg"} />
                     <img src={"/src/assets/Home/Home_M.jpg"} alt="Logo of Ziiemli" />
@@ -20,13 +20,11 @@ const Home = () => {
                         <strong>CHAMPANHET</strong>
                     </h1>
                     <p>GAFFER</p>
-                    <a href="">DISCOVER</a>
+                    <a className="home__top__name__discover" href="">DISCOVER</a>
                 </div>
             </section>
-            <section>
-                {dataCategories && dataCategories.map((category, index) => (
-                    <HomeCategory to={category.to} img_M={category.img_M} img_D={category.img_D} title={category.title} key={index} />
-                ))}
+            <section className="home__categories">
+                {dataCategories && dataCategories.map((category, index) => <HomeCategory to={category.to} img_M={category.img_M} img_D={category.img_D} title={category.title} key={index} />)}
             </section>
         </main>
     )
